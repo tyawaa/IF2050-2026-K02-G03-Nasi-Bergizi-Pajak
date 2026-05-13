005f81db77cd01f615c16633bb613bf810a7c63f
ibr4him-ctrl <18224120@std.stei.itb.ac.id>
2026-05-13 13:40:47 +0700

    fix: update default database credentials for local development

--- src/main/java/nasi_bergizi_pajak/config/DatabaseConnection.java
+++ src/main/java/nasi_bergizi_pajak/config/DatabaseConnection.java
@@ -10,8 +10,8 @@ public class DatabaseConnection {
             + "&useSSL=false"
             + "&allowPublicKeyRetrieval=true"
             + "&serverTimezone=Asia/Jakarta";
-    private static final String DEFAULT_DB_USER = "nbp_app";
-    private static final String DEFAULT_DB_PASSWORD = "nbp1234";
+    private static final String DEFAULT_DB_USER = "root";
+    private static final String DEFAULT_DB_PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         String dbUrl = getConfig("DB_URL", "db.url", DEFAULT_DB_URL);
